f6013af37266ae20af6e494863fd41b8c2c7810f
TemplarRei <boris.stoyanovv@gmail.com>
2016-01-07 13:52:03 +0200

    merge commit

--- server/data/models/User.js
+++ server/data/models/User.js
@@ -1,80 +1,80 @@
-let mongoose = require('mongoose'),
-    encryption = require('../../utilities/encryption'),
-    tagRegex = /<[A-Za-z\s]+>.*<\/[A-Za-z\s]+>|<[A-Za-z\s]+\/\s*>/g,
-    mailRegex = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
+(function () {
+    'use strict';
 
-let Schema = mongoose.Schema;
+    let mongoose = require('mongoose'),
+        encryption = require('../../utilities/encryption'),
+        tagRegex = /<[A-Za-z\s]+>.*<\/[A-Za-z\s]+>|<[A-Za-z\s]+\/\s*>/g,
+        mailRegex = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
 
-module.exports.init = function () {
-    var userSchema = new Schema({
-        id: Schema.Types.ObjectId,
-        username: {
-            type: String, 
-            require: '{PATH} is required', 
-            unique: true,
-            maxlength: 30,
-            minlength: 6,
-            validation: function(val) {
-                return tagRegex.test(val);
-            }
-        },
-        email: {
-            type: String, 
-            require: '{PATH} is required', 
-            unique: true,
-            validation: function (val) {
-                return mailRegex.test(val);
-            }
-        },
-        firstName: {
-             type: String, 
-            require: '{PATH} is required', 
-            unique: true,
-            maxlength: 30,
-            minlength: 6,
-            validation: function(val) {
-                return tagRegex.test(val);
-            }
-        },
-        lastName: {
-             type: String, 
-            require: '{PATH} is required', 
-            unique: true,
-            maxlength: 30,
-            minlength: 6,
-            validation: function(val) {
-                return tagRegex.test(val);
-            }
-        },
-        birthDate: {
-            type: Date
-            // TODO: Validation?
-        },
-        sex: Boolean,
-        friends: {
-            Type: Array,
-            // TODO: learn how to make relations in Mongoose. 
-        },
-        posts: {
-            Array,
-            // TODO: learn how to make relations in Mongoose.
-        },
-        salt: String,
-        hashPass: String
-    });
+    let Schema = mongoose.Schema;
 
-    userSchema.method({
-        authenticate: function (password) {
-            if (encryption.generateHashedPassword(this.salt, password) === this.hashPass) {
-                return true;
+    module.exports.init = function () {
+        var userSchema = new Schema({
+            id: Schema.Types.ObjectId,
+            username: {
+                type: String,
+                require: '{PATH} is required',
+                unique: true,
+                maxlength: 30,
+                minlength: 6,
+                validation: function (val) {
+                    return tagRegex.test(val);
+                }
+            },
+            email: {
+                type: String,
+                require: '{PATH} is required',
+                unique: true,
+                validation: function (val) {
+                    return mailRegex.test(val);
+                }
+            },
+            firstName: {
+                type: String,
+                require: '{PATH} is required',
+                unique: true,
+                maxlength: 30,
+                minlength: 6,
+                validation: function (val) {
+                    return tagRegex.test(val);
+                }
+            },
+            lastName: {
+                type: String,
+                require: '{PATH} is required',
+                unique: true,
+                maxlength: 30,
+                minlength: 6,
+                validation: function (val) {
+                    return tagRegex.test(val);
+                }
+            },
+            birthDate: {
+                type: Date
+                // TODO: Validation?
+            },
+            sex: Boolean,
+            friends: {
+                Type: [userSchema]
+            },
+            // posts: {
+            //     Type: [postSchema]
+            // },
+            salt: String,
+            hashPass: String
+        });
+
+        userSchema.method({
+            authenticate: function (password) {
+                if (encryption.generateHashedPassword(this.salt, password) === this.hashPass) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
             }
-            else {
-                return false;
-            }
-        }
-    });
+        });
 
-    var User = mongoose.model('User', userSchema);
-};
-
-
+        var User = mongoose.model('User', userSchema);
+    };
+} ());
