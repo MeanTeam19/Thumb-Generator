63e3be5379f94844d213b5eae085bb9211dedbab
TemplarRei <boris.stoyanovv@gmail.com>
2016-01-11 17:13:25 +0200

    Initial work on the admin panel.

---
+++ server/controllers/AdminController.js
@@ -0,0 +1,55 @@
+var encryption = require('../utilities/encryption');
+var users = require('../data/users');
+
+var CONTROLLER_NAME = 'admin';
+
+module.exports = {
+    postRegister: function (req, res, next) {
+        console.log(req.body);
+        var newUserData = req.body;
+
+        if (newUserData.password != newUserData.confirmPassword) {
+            req.session.error = 'Passwords do not match!';
+            res.redirect('/register');
+        }
+        else {
+            
+            console.log(newUserData);
+            newUserData.salt = encryption.generateSalt();
+            
+            newUserData.hashPass = encryption.generateHashedPassword(newUserData.salt, newUserData.password);
+            users.create(newUserData, function(err, user) {
+                if (err) {
+                    console.log(err);
+                    if (err.code === 11000) {
+                        req.session.error = 'This user is taken :(';
+                    } else {
+                        req.session.error = 'We couldn\'t register you... Please, try again :)';
+                    }
+
+                    res.redirect('/register');
+                    return;
+                }
+
+                req.logIn(user, function (err) {
+                    if (err) {
+                        res.status(400);
+                        req.session.error = err.toString();
+                        res.redirect('/login');
+                    }
+                    else {
+                        res.redirect('/');
+                    }
+                });
+            });
+        }
+    },
+    getLogin: function (req, res, next) {
+        if (req.user) {
+            res.redirect('/');
+            return;
+        }
+
+        res.render(CONTROLLER_NAME + '/login');
+    }
+};
--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,7 +1,10 @@
 var usersController = require('./users-controller'),
   statisticsController = require('./statistics-controller');
+var AdminController = require('./AdminController');
 
 module.exports = {
+    users: UsersController,
   users: usersController,
   statistics: statisticsController
+    admin: AdminController
 };
